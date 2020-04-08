81944c222ce10cfe1d10075ebc18f46df6b01bf1
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-07 22:06:49 -0400

    [Abhishek] - Initial commit

---
+++ app/src/main/java/com/aby/c0769778_w2020_mad3125_midterm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aby.c0769778_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
